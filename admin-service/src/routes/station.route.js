fd362fa694323ca8ace5bb1ff0b821ffb2b41e82
Titu022 <js2125892@gmail.com>
2026-07-08 23:11:06 +0530

    added the services and routes

--- admin-service/src/routes/station.route.js
+++ admin-service/src/routes/station.route.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
 const {getUserContext} = require('../middlewares/getUserContext.middleware');
-const {createStation} = require('../controllers/station.controller');
+const {createStation, getAllStations, getStationById} = require('../controllers/station.controller');
 
 const router = express.Router();
+
+router.get("/station/internal/:stationId", internalAuth, getStationByIdInternal);
+
+router.get("/station", getUserContext, getAllStations);
+router.get("/station/:stationId", getUserContext, getStationById);
 
 router.post("/station", getUserContext, createStation);
 
--- admin-service/src/routes/train.route.js
+++ admin-service/src/routes/train.route.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const {getUserContext} = require('../middlewares/getUserContext.middleware');
-const {createTrain} = require('../controllers/train.controller');
+const {createTrain, getAllTrains, getTrainById} = require('../controllers/train.controller');
 
 const router = express.Router();
 
 router.post('/train', getUserContext, createTrain);
+
+router.get("/train", getUserContext, getAllTrains);
+router.get("/train/:trainId", getUserContext, getTrainById);
 
 module.exports = router;
